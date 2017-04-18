3cb4498ca1761686a9d90a444a1fc01fa1b51449
Lachezar Ivanov <luchobg@abv.bg>
2017-04-18 17:50:44 +0300

    getting json for tracks but doesn't show up in textview for some reason'

--- app/src/main/java/com/example/rahovec/f1project/Tracks.java
+++ app/src/main/java/com/example/rahovec/f1project/Tracks.java
@@ -1,13 +1,79 @@
 package com.example.rahovec.f1project;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Tracks extends AppCompatActivity {
+
+    String JSON_STRING;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracks);
+        new BackgroundTask().execute();
+    }
+
+
+
+    class BackgroundTask extends AsyncTask<Void,Void,String> {
+
+        String json_url;
+
+        @Override
+        protected void onPreExecute() {
+            json_url = "http://ergast.com/api/f1/circuits.json";
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            try {
+                URL url =  new URL(json_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while((JSON_STRING = bufferedReader.readLine()) != null){
+
+                    stringBuilder.append(JSON_STRING+"\n");
+                }
+
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+
+                return stringBuilder.toString().trim();
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView textView = (TextView) findViewById(R.id.tracksView);
+            textView.setText(result);
+        }
     }
 }
