ceaa3c2dd28124b834ee67f3c40781480af258df
Rahovec <konstantin.todorov98@gmail.com>
2017-05-09 00:01:07 +0300

    Added WebView to driver wins

--- app/src/main/java/com/example/rahovec/f1project/DriverWins.java
+++ app/src/main/java/com/example/rahovec/f1project/DriverWins.java
@@ -2,6 +2,7 @@ package com.example.rahovec.f1project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class DriverWins extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class DriverWins extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_wins);
+
+        String url = "http://ergast.com/api/f1/results/1/drivers";
+        WebView view = (WebView) this.findViewById(R.id.driver_wins);
+        view.loadUrl(url);
     }
 }
---
+++ app/src/main/java/com/example/rahovec/f1project/MyAppWebViewClient.java
@@ -0,0 +1,8 @@
+package com.example.rahovec.f1project;
+
+/**
+ * Created by rahovec on 02.05.17.
+ */
+
+public class MyAppWebViewClient {
+}
